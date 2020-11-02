9b99112d171b9b7d2a43421979884cc7432278c5
KARBOWSKI Piotr <piotr.karbowski@gmail.com>
2020-11-02 17:24:59 +0100

    main.py: fix indetion of emerge_result.

--- src/pkg_testing_tool/main.py
+++ src/pkg_testing_tool/main.py
@@ -205,8 +205,8 @@ def run_testing(job, args):
             else:
                 env['FEATURES'] = " ".join(global_features)
 
-            emerge_result = subprocess.run(emerge_cmdline, env=env)
-            print('')
+        emerge_result = subprocess.run(emerge_cmdline, env=env)
+        print('')
 
     return {
         'use_flags': " ".join(job['use_flags']),
